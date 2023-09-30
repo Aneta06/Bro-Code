1900165fdebdddb3a64fdb2f503e2bf35f06e88a
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-09-30 15:00:27 +0200

    ArrayCars

---
+++ ArrayCars.java
@@ -0,0 +1,15 @@
+public class ArrayCars {
+
+    public static void main(String[] args) {
+
+        // array = used to store multiply values in a single variable
+        //       = slouží k uložení násobených hodnot do jedné proměnné
+
+        String [] cars = {"BMW", "Volvo", "Audi"}; // elementy = ty nazvy a pocitame od nuly v zavorce je (0,1,2) elementu
+
+        cars [0] = "Skoda"; // ted jsem prepsala element 0 coz je BMW na Skoda
+
+        System.out.println(cars[0]);
+
+    }
+}
