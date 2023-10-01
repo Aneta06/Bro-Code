44a6f2b2cc773762fdcece76a016d9153ce2e8e4
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 08:13:48 +0200

    Pocitani2Math

---
+++ Pocitani2Math.java
@@ -0,0 +1,18 @@
+public class Pocitani2Math {
+
+    public static void main (String[] args){
+
+        double x = 3.14;
+        double y = -10;
+        double b = 8;
+
+        double z = Math.max(x, y); // tohle porovna jake cislo je vetsi  a vypise to cilo, Math.min, zas to nejnizzsi
+        System.out.println(z);
+
+       b = Math.sqrt(b); // vysledek je
+        System.out.println(b);
+
+
+    }
+
+}
