e2ab302c1540167d3618bb7bcc8d7da7bb3f23de
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 20:39:55 +0200

    FinalKeyword

---
+++ FinalKeyword.java
@@ -0,0 +1,11 @@
+public class FinalKeyword {
+
+    public static void main(String[] args) {
+
+        // final = not possible to change // vetsinou piseme s velkyma pismenama tu promennou
+
+        final double PI = 3.14159;
+
+        System.out.println(PI);
+    }
+}
