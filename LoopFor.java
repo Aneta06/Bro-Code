0e22913a9da7978c9b0adbddb8079b4aa0f23378
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 08:12:21 +0200

    LoopFor

---
+++ LoopFor.java
@@ -0,0 +1,21 @@
+public class LoopFor {
+
+    public static void main(String[] args) {
+
+        // FOR LOOP = execute a block of code a limited amount of times
+        //          = spustit blok kódu v omezeném počtu opakování
+
+        for(int i = 0; i <= 10; i++){ // kdyz chci aby mi pricitalo po dvou, tak pisu i+=2
+            System.out.println(i);
+
+        }
+
+        for (int i = 10; i >=0; i--) { // kdyz chci aby mi odecitalo po dvou pisu i-=2
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year");
+    }
+
+
+
+}
