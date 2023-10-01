7658d8f568f98999335fe8c02f0543229c53ccad
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-01 18:44:58 +0200

    wrapperClass

---
+++ wrapperClass.java
@@ -0,0 +1,30 @@
+public class wrapperClass {
+
+    public static void main(String[] args) {
+
+        //Wrapper Class = provides a way to use primitive data types as reference data types reference data
+                        // types contain useful methods can be used with collections (ex.ArrayList)
+
+
+           //poskytuje způsob, jak používat primitivní datové typy jako referenční datové typy referenční
+           // datové typy obsahují užitečné metody, které lze použít s kolekcemi
+
+             // primitive           // wrapper
+             // - - - - -           //-------
+            // boolean               // Boolean
+            // char                 // Character
+           // int                   // Integer
+          // double                 //Double
+
+          //autoboxing = the automatic conversion that the Java complier makes between the primitive
+                          // types and their coresponding object wrapper class
+          // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primite
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Aneta";
+
+    }
+}
